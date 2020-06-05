b48b6fd467ce6aec6bb69a24d4615e65e5b1203a
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-06-05 12:58:00 -0700

    Added another axios call that finds the followers, and set the spporpriate state to this data

--- react-user/src/App.js
+++ react-user/src/App.js
@@ -15,8 +15,21 @@ class App extends React.Component {
     axios.get('https://api.github.com/users/Aaron-Lamb')
     .then(response => {
       console.log(response.data)
+      this.setState({ profile: response.data})
+      console.log(this.state.profile)
     })
     .catch(error => console.log(error))
+
+    axios.get('https://api.github.com/users/Aaron-Lamb/followers')
+    .then(response => {
+      this.setState({ followers: response.data})
+      console.log(this.state)
+    })
+    .catch(error => console.log(error))
+  }
+
+  componentDidUpdate() {
+    console.log('Updated');
   }
 
   render(){
