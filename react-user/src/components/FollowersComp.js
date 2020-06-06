d3a2db2351876ea0c08e0cca8cb1a28a2221f2c7
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-06-05 17:19:52 -0700

    Added a font from google fonts

--- react-user/src/components/FollowersComp.js
+++ react-user/src/components/FollowersComp.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './components.css';
 
 const FollowersComp = props => {
     return(
         <div>
             {props.followers.map(profile => {
                 return(
-                    <div key={profile.id}>
+                    <div key={profile.id} className='profile'>
                         <img src={profile.avatar_url} alt={profile.login} />
                         <h5>{profile.login}</h5>
                     </div>
