c77faee26b0dd5deecd047b62767d3364e3c183e
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-06-05 17:51:39 -0700

    Almost done styling

--- react-user/src/components/FollowersComp.js
+++ react-user/src/components/FollowersComp.js
@@ -3,12 +3,12 @@ import './components.css';
 
 const FollowersComp = props => {
     return(
-        <div>
+        <div className='followlist'>
             {props.followers.map(profile => {
                 return(
-                    <div key={profile.id} className='profile'>
+                    <div key={profile.id} className='followers'>
                         <img src={profile.avatar_url} alt={profile.login} />
-                        <h5>{profile.login}</h5>
+                        <h5 className='login'>{profile.login}</h5>
                     </div>
                 )
             })}
